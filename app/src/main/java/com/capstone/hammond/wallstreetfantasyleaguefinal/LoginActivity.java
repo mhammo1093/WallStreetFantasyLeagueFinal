0c1806756a1d71ff2da8f17c8e4aca9fed002479
mhammo1093 <michaham@umflint.edu>
2015-03-30 11:53:10 -0400

    Updated to include navigation drawer.

--- app/src/main/java/com/capstone/hammond/wallstreetfantasyleaguefinal/LoginActivity.java
+++ app/src/main/java/com/capstone/hammond/wallstreetfantasyleaguefinal/LoginActivity.java
@@ -319,7 +319,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             showProgress(false);
             if (success) {
                 finish();
-                startActivity(new Intent("HomeScreenActivity"));
+                startActivity(new Intent("NavDrawer"));
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
