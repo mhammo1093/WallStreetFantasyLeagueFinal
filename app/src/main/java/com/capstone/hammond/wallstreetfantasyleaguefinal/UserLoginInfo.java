95cd73e4f7c417371e8e1f1743aa1fa877896a67
mhammo1093 <michaham@umflint.edu>
2015-04-04 17:03:43 -0400

    Updated with Account Fragment (4.4.2015)

---
+++ app/src/main/java/com/capstone/hammond/wallstreetfantasyleaguefinal/UserLoginInfo.java
@@ -0,0 +1,13 @@
+package com.capstone.hammond.wallstreetfantasyleaguefinal;
+
+/**
+ * Created by Mike on 4/4/2015.
+ */
+
+public class UserLoginInfo extends LoginActivity {
+    public static String userEmail;
+
+    public UserLoginInfo(String mEmail) {
+        String userEmail = mEmail;
+    }
+}
